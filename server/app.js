83055d0f1bab3a15bb0440f172a828bfcfef8bff
shubham-arutwar <shubhamarutwar23@gmail.com>
2022-06-14 17:08:18 +0530

    connected to MongoDB atlas, working backend

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://localuser:abcxyz123@graphql.zruwr.mongodb.net/library?retryWrites=true&w=majority');
+mongoose.connection.once('open',() =>{
+    console.log('connected to ddatabase');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
