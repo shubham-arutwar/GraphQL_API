55248736af54529d3b60cc49490f7ea890bc21db
shubham-arutwar <shubhamarutwar23@gmail.com>
2022-06-16 17:48:36 +0530

    working API, bugs in resolver, cors issue fixed

--- server/app.js
+++ server/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors=require("cors");
 
 const app = express();
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ };
 
 mongoose.connect('mongodb+srv://localuser:abcxyz123@graphql.zruwr.mongodb.net/library?retryWrites=true&w=majority');
 mongoose.connection.once('open',() =>{
     console.log('connected to ddatabase');
-})
+});
+
+app.use(cors(corsOptions));
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -18,3 +27,4 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
 });
+
